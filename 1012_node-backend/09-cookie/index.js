a3ccf6e761f589addf5aa9d16d186e09de2bf166
DJ94DJ <unistudio94@gmail.com>
2023-11-04 17:28:12 +0900

    study : session 이론 및 실습

--- 1012_node-backend/09-cookie/index.js
+++ 1012_node-backend/09-cookie/index.js
@@ -7,37 +7,45 @@ app.set("view engine", "ejs");
 app.use(cookieParser()); // 쿠키를 해석할 수 있게 해줌.
 // 쿠키 파서 미들웨어 걸기 = 쿠키를 해석하고 사용할 수 있게함.
 
-
-
 const cookieConfig = {
-    // httpOnly: true, // document.cookie로의 접근을 막는다.(작동시 콘솔안보임) 서버에서만 접근가능하게됨.
-   // 미들웨어에서이름 바꾸면 안됨.
-    maxAge: 10000, // (ms(1000=1초) 단위로 보존기간 설정) 24시간 설정시 1000*60*60*24로 설정
-    // expires: "2023-11=04T18:00"
-    //path : "/" // 만약 "/test" -> localhost:8000 *(해당 주소엔 쿠키 존재 안함 대신 /test에 포진됨 )
-    // secure: true, // https 보안 서버에서만 쿠키동작
-    // signed: true // 쿠키 암호화  -> req.signedCookies
+  // httpOnly: true, // document.cookie로의 접근을 막는다.(작동시 콘솔안보임) 서버에서만 접근가능하게됨.
+  // 미들웨어에서이름 바꾸면 안됨.
+  maxAge: 10000, // (ms(1000=1초) 단위로 보존기간 설정) 24시간 설정시 1000*60*60*24로 설정
+  // expires: "2023-11=04T18:00"
+  //path : "/" // 만약 "/test" -> localhost:8000 *(해당 주소엔 쿠키 존재 안함 대신 /test에 포진됨 )
+  // secure: true, // https 보안 서버에서만 쿠키동작
+  // signed: true // 쿠키 암호화  -> req.signedCookies
 };
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const noPopup = req.cookies.popup; //오류있을지 주석
+  res.render("index", { noPopup });
 });
 
 app.get("/set", (req, res) => {
-//서버가 쿠키를 만들어서 응답을 보냄
-    // key: "key1  value: value1
-    // res.cookie("key1", "value1", cookieConfig);
-    // res.cookie("popup", "1", cookieConfig);
-    res.cookie("popupClosed", "true", cookieConfig);
-    res.send("set cookie");
+  //서버가 쿠키를 만들어서 응답을 보냄
+  // key: "key1  value: value1
+  // res.cookie("key1", "value1", cookieConfig);
+  // res.cookie("popup", "1", cookieConfig);
+  res.cookie("popupClosed", "true", cookieConfig);
+  res.send("set cookie");
 });
 
 app.get("/get", (req, res) => {
-    console.log("cookie :",req.cookies);
-    res.send(req.cookies);
+  console.log("cookie :", req.cookies);
+  res.send(req.cookies);
 });
-
 
 app.listen(PORT, function () {
   console.log("Sever Open:", PORT);
+});
+
+// 해답
+
+app.post("/setCookie", (req, res) => {
+  //쿠키는 응답을 보내거나 하지 않는다. 응답을 받을 뿐이다.
+  res.cookie("popup", "y", cookieConfig);
+
+  // 따라서 응답을 따로 해야한다
+  res.send("set cookie result", { result: true });
 });
---
+++ 1012_node-backend/10-session/index.js
@@ -0,0 +1,82 @@
+const { createSecretKey } = require("crypto");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const session = require("express-session");
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: "secret key",
+    resave: false, //모든 요청마다 session을 다시 저장할 건자
+    saveUnitialized: true, //클라이언트 처음접속시 세션 초기화 여부
+    cookie: {
+      httpOnly: true, //document.cookie로는 접속 안됨
+      maxAge: 10 * 1000,
+    },
+    // secure: true //https에서만 동작하게함
+  })
+);
+
+// 인덱스 (user가 로그인한 경우 nav에 login or logout 이 떠있어야함)
+app.get("/", (req, res) => {
+  res.render("index", { user: req.session.user });
+
+  if (req.session.user === undefined) {
+    console.log("현재 비 로그인 상태입니다.");
+  } else console.log({ user: req.session.user });
+});
+
+//회원정보 세션 생성하는 라우터
+// 로그인 버튼 클릭시 Axios로 전달
+app.get("/set", (req, res) => {
+  console.log("1:", req.session);
+
+  app.get("/get", (req, res) => {
+    console.log(1);
+  }); // 로그인 성공한 시점에 req.session.user 에 고유한 값
+
+  req.session.user = "lily";
+  //console.log("2:", req.session); //클라이언트 별로 세션을 가지고 있기에 req를 쓴다.
+  res.send("set session ");
+  console.log(req.session.user, "회원 로그인 완료");
+});
+
+app.get("/set2", (req, res) => {
+  console.log("2:", req.session);
+
+  app.get("/get2", (req, res) => {
+    console.log(2);
+  }); // 로그인 성공한 시점에 req.session.user 에 고유한 값
+  req.session.user = "DJ";
+  res.send("set session ");
+  console.log(req.session.user, "회원 로그인 완료");
+});
+
+// 회원 로그아웃 라우터
+// app.get("/logout", (req, res) => {
+//   console.log(req.session.user, "회원 로그아웃 진행");
+
+//   req.session.destroy(); // 세션 삭제
+
+//   res.send("logout");
+// });
+
+app.get("/logout", (req, res) => {
+  console.log(req.session.user, "회원 로그아웃 진행");
+  req.session.destroy((err) => {
+    if (err) throw err;
+
+    res.send({ result: true, msg: "로그아웃 성공" });
+  });
+});
+
+// 회원정보 알림 라우터
+// app.get("/get", (req, res) => {
+//   console.log("user :", req.session.user);
+//   res.send({ user: req.session.user });
+// });
+
+app.listen(PORT, function () {
+  console.log("Sever Open:", PORT);
+});
